8cf11b53255cab65f7c47ac1dd2cafd6facdec35
Lewi Uberg <43423020+lewiuberg@users.noreply.github.com>
2021-04-14 00:10:35 +0200

    install nerd fonts

--- New setup/terminal.sh
+++ New setup/terminal.sh
@@ -6,3 +6,7 @@ curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | 
 # Install Powerlevel10k
 brew install romkatv/powerlevel10k/powerlevel10k
 echo 'source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+# Install nerd fonts
+brew tap homebrew/cask-fonts
+brew install --cask font-meslo-lg-nerd-font
