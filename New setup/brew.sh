164ec3b2dbcaf4e8cc2234afd2f9a8ecdace8b45
Lewi Uberg <43423020+lewiuberg@users.noreply.github.com>
2022-01-10 07:39:27 +0100

    Update brew.sh

--- New setup/brew.sh
+++ New setup/brew.sh
@@ -2,35 +2,59 @@
 
 # Install Brew Packages
 brew install pwgen
-brew install virtualenv
-brew install poetry
-brew install tree
 brew install node
+brew install virtualenv
+brew install pyenv
+brew install pyenv-virtualenv
+brew install poetry
 brew install git
+brew install git-lfs
 brew install gh
+brew install tree
+brew install graphviz
+brew install nightlight
+brew install direnv
 brew install docker
 brew install docker-machine
 brew install azure-cli
-brew install direnv
 
 # Install MacOS Applications
-brew cask install visual-studio-code
-brew cask install hyperdock
-brew cask install sourcetree
-brew cask install vnc-viewer
-brew cask install parallels
-brew cask install parallels-access
-brew cask install parallels-client
-brew cask install parallels-toolbox
-brew cask install slack
-brew cask install discord
-brew cask install messenger
-brew cask install microsoft-teams
-brew cask install grammarly
-brew cask install cheatsheet
-brew cask install jabref
-brew cask install autodesk-fusion360
-brew cask install raspberry-pi-imager
-brew cask install balenaetcher
-brew cask install pibakery
-brew cask install applepi-baker
+brew install --cask iterm2
+brew install --cask syntax-highlight
+brew install --cask qlmarkdown
+brew install --cask qlstephen
+brew install --cask parallels
+brew install --cask parallels-access
+brew install --cask parallels-client
+brew install --cask parallels-toolbox
+brew install --cask vnc-viewer
+brew install --cask virtualbox
+brew install --cask termius
+brew install --cask transmission
+brew install --cask fluid
+brew install --cask avast-security
+brew install --cask 
+brew install --cask 
+brew install --cask autodesk-fusion360
+brew install --cask blender
+brew install --cask visual-studio-code
+brew install --cask prince
+brew install --cask github
+brew install --cask sourcetree
+brew install --cask slack
+brew install --cask discord
+brew install --cask messenger
+brew install --cask jabref
+brew install --cask grammarly
+brew install --cask microsoft-office
+brew install --cask microsoft-teams
+brew install --cask postman
+brew install --cask notable
+brew install --cask raspberry-pi-imager
+brew install --cask balenaetcher
+brew install --cask applepi-baker
+
+# Install casks of drivers
+brew tap homebrew/cask-drivers
+brew install --cask logitech-options
+brew install --cask philips-hue-sync
